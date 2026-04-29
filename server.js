94a6460e39d71647cfa250e8bbf5d923d43dada9
Shyam Kumar <164172271+shyamkumar18@users.noreply.github.com>
2026-04-29 20:33:13 +0530

    Refactor server.js to use environment variables

--- server.js
+++ server.js
@@ -5,23 +5,19 @@ const authRoutes = require("./routes/Auth");
 const postRoutes = require("./routes/posts");
 
 const app = express();
-
-const port = 3000;
-
-secret ="ace1812"
+const port = process.env.PORT || 3000;
+const secret = process.env.JWT_SECRET || "ace1812";
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI || "mongodb+srv://shyam18:shyamkumar18@cluster0.9tzmpza.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Could not connect to MongoDB", err));
 
-mongoose.connect('mongodb+srv://shyam18:shyamkumar18@cluster0.9tzmpza.mongodb.net/')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
-
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
-
-
-app.listen(port, () => 
-    console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
